6923c4c5196dbfa280a52d62d572091f92902c19
Unknown <luniderelias@gtel.ufc.br>
2018-02-11 15:45:46 -0300

    Beginning Development

---
+++ VirtualCloset/app/src/main/java/thinkapps/com/virtualcloset/View/Home/MainActivity.java
@@ -0,0 +1,15 @@
+package thinkapps.com.virtualcloset.View.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import thinkapps.com.virtualcloset.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
